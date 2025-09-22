a7d6031dbe5af0561e8013c3eaf011bca2cbbc24
phj192 <parkhaejun0921@gmail.com>
2025-09-22 19:27:45 +0900

    feat: flask app 계획서 완성

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return "Hello, Flask!"
 
-@app.get("/home")
+"""@app.get("/home")
 def home():
-    return render_template("home.html", title="Flask 템플릿 연결")
+    return render_template("home.html", title="Flask 템플릿 연결")"""
+
+@app.get("/hw1")
+def home():
+    return render_template("hw1.html", title="강의실 배정 건물 예측")
 
 if __name__ == "__main__":
     app.run()
